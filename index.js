b7354cd282d4157113a6b598ca09417e85dbdd2c
Decon123 <deconkelaart@gmail.com>
2023-07-20 04:47:24 +0530

    Created DynamoDB

--- index.js
+++ index.js
@@ -1,89 +1,101 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
-//Interact with AWS IOT Services
-const awsIot = require('aws-iot-device-sdk');
-//
 const app = express();
 const path = require('path');
+const AWS = require('aws-sdk');
+const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb');
+const { DynamoDB } = require('@aws-sdk/client-dynamodb');
 
 const http = require('http').Server(app);
-const io = require('socket.io')(http)
+const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Creating an AWS IoT device object:
-/* const device = awsIot.device({
-    keyPath: '<path_to_private_key>',
-    certPath: '<path_to_certificate>',
-    caPath: '<path_to_root_ca>',
-    clientId: 'uniqueClientIdentifier',
-    host: 'your-aws-iot-endpoint.amazonaws.com'
-});  */
-    
+// Set the AWS region
+AWS.config.update({ region: 'us-east-1' });
+
+// Create an instance of DynamoDB DocumentClient
+const docClient = DynamoDBDocument.from(new DynamoDB());
+
 let trashCanFilledPercentage = 0;
 
-app.get('/trashCanFilledPercentage', function (req, res) {
-    res.write(String(trashCanFilledPercentage));
-    res.end();
+app.get('/trashCanFilledPercentage', (req, res) => {
+  res.write(String(trashCanFilledPercentage));
+  res.end();
 });
 
-/*-------------------------------------------------
-// Connect to AWS IoT Core
-device.on('connect', function() {
-    console.log('Connected to AWS IoT Core');
-  
-    // Subscribe to a topic
-    device.subscribe('topic/trashCanFilledPercentage');
-  });
-  
-  // Handle received messages
-  device.on('message', function(topic, payload) {
-    const trashCanFilledPercentage = Number(payload.toString());
-    console.log('Received trashCanFilledPercentage:', trashCanFilledPercentage);
-  
-    // Update the value and emit to connected clients
-    // ...
-  });
-  
-  // Handle errors
-  device.on('error', function(error) {
-    console.error('AWS IoT error:', error);
-  });
-  
-        /*-------------------------------------------------*/
+// Subscribe to DynamoDB Stream events
+const subscribeToStreamEvents = async (streamArn) => {
+  try {
+    // Create a DynamoDB Document Client
+    const dynamodb = new AWS.DynamoDB();
 
-app.post('/trashCanFilledPercentage', function (req, res) {
-    trashCanFilledPercentage = Number(req.body.value);
+    // Create a stream event source
+    const streamParams = {
+      TableName: 'tbl_real_data', // Replace with your table name
+      StreamArn: streamArn
+    };
 
-    io.emit('trashCanFilledPercentageUpdate', trashCanFilledPercentage);
+    // Start listening to stream events
+    const stream = dynamodb.createEventStream(streamParams);
 
-    //send data to influxDB(Install influxdb to server)
+    // Process incoming stream records
+    stream.on('data', (record) => {
+      const eventName = record.eventName;
+      const dynamodbNewImage = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
 
-    //make a series using influxDB
+      // Perform actions based on the event type
+      if (eventName === 'INSERT' || eventName === 'MODIFY') {
+        // Handle insert or modify event
+        trashCanFilledPercentage = Number(dynamodbNewImage.filledPercentage);
+        io.emit('trashCanFilledPercentageUpdate', trashCanFilledPercentage);
+        console.log('Updated trash fill percentage:', trashCanFilledPercentage);
+      } else if (eventName === 'REMOVE') {
+        // Handle remove event
+        // You can implement custom logic if needed when an item is removed
+        console.log('Item removed:', dynamodbNewImage);
+      }
+    });
 
-    console.log('Updated trashfill percentage: ' + trashCanFilledPercentage);
-    
-    res.write('updated the percentage');
-    res.end();
+    stream.on('error', (err) => {
+      console.error('Error processing DynamoDB Stream events:', err);
+    });
+  } catch (err) {
+    console.error('Failed to subscribe to DynamoDB Stream events:', err);
+  }
+};
+
+// Specify the DynamoDB Stream ARN
+const streamArn = 'arn:aws:dynamodb:us-east-1:889028696898:table/tbl_real_data/stream/2023-07-20T09:00:00.000';
+
+// Call the function to subscribe to DynamoDB Stream events
+subscribeToStreamEvents(streamArn);
+
+app.post('/trashCanFilledPercentage', (req, res) => {
+  trashCanFilledPercentage = Number(req.body.value);
+
+  io.emit('trashCanFilledPercentageUpdate', trashCanFilledPercentage);
+
+  console.log('Updated trash fill percentage:', trashCanFilledPercentage);
+
+  res.write('Updated the percentage');
+  res.end();
 });
-    
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use('/js', express.static('js'))
+app.use('/js', express.static('js'));
 
 const port = process.env.PORT || 3000;
 
 const server = http.listen(port, () => {
-    const { port } = server.address();
-    console.log(`Server running on http://3.91.54.203:${port}`);
+  console.log(`Server running on http://3.91.54.203:${port}`);
 });
 
-
-io.on('connection', function (socket) {
-    socket.emit('trashCanFilledPercentageUpdate', trashCanFilledPercentage);
-    console.log('Client connected...')
-});   
+io.on('connection', (socket) => {
+  socket.emit('trashCanFilledPercentageUpdate', trashCanFilledPercentage);
+  console.log('Client connected...');
+});
